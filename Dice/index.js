567aaece62ea355b42e7d02cbfd35d68a29749a5
Maria <mariaalkholany@gmail.com>
2026-09-23 00:20:32 +0300

    Add button to the Dice Game

--- Dice/index.js
+++ Dice/index.js
@@ -1,3 +1,6 @@
+
+
+function roleDice(){
 var randomNumber1= Math.floor(Math.random()*6)+1;
 var randonImg1="./images/dice"+randomNumber1+".png";
 
@@ -18,4 +21,5 @@ else if(randomNumber1>randomNumber2)
 }
 else{
     document.querySelector("h1").textContent="PLayer2🎉 is the winner!";
+}
 }
